61d63b393f64037f96b285396911408e23715618
Jaewon Choi <79743430+jaewonchoi1234@users.noreply.github.com>
2024-04-24 15:49:57 +0900

    level1 요구사항2

--- src/APP.java
+++ src/APP.java
@@ -12,7 +12,7 @@ public class APP {
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
 
-        System.out.println(firstInt+" "+operator+" "+secondInt);
         
+
     }
 }
