4c90090879d628db2c39bffafb19395182765947
Jaewon Choi <79743430+jaewonchoi1234@users.noreply.github.com>
2024-04-30 17:44:44 +0900

    level2 요구사항10

--- src/ArithmeticCalculator.java
+++ src/ArithmeticCalculator.java
@@ -1,15 +1,18 @@
 
 public class ArithmeticCalculator extends Calculator {
-    AddOperator add = new AddOperator();
-    SubtractOperator sub = new SubtractOperator();
-    MultiplyOperator mul = new MultiplyOperator();
-    DivideOperator div = new DivideOperator();
+    AddOperator add;
+    SubtractOperator sub;
+    MultiplyOperator mul;
+    DivideOperator div;
+    ModOperator mod;
 
     public ArithmeticCalculator() {
+
         add = new AddOperator();
         sub = new SubtractOperator();
         mul = new MultiplyOperator();
         div = new DivideOperator();
+        mod = new ModOperator();
     }
 
     public double calculate (int firstNum,int secondNum,char operator) throws ArithmeticException, IllegalArgumentException {
@@ -29,6 +32,12 @@ public class ArithmeticCalculator extends Calculator {
                     throw new ArithmeticException("0으로 나누기를 할 수 없습니다.");
                 }
                 result=div.operate(firstNum,secondNum);
+                break;
+            case '%':
+                if (secondNum == 0) {
+                    throw new ArithmeticException("0으로 나누기를 할 수 없습니다.");
+                }
+                result=mod.operate(firstNum,secondNum);
                 break;
             default:
                 throw new IllegalArgumentException("입력한 기호가 사칙연산 기호가 아닙니다.");
